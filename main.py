e24bcb201925fb04f035fe55f381f9fc8bd10b92
KlamFy <103330222+AleksKlamfy@users.noreply.github.com>
2022-07-15 14:15:09 +0300

    Update main.py
    Add function to auto roles.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from discord.ext import commands
 import discord
 TOKEN = 'OTg5MTg1NzU1Njg1NDU4MDIw.G7xUAU.EoxWxVvw7mYT6GA43fdzEee7EnQxAYLi4CQykk'
+bot = commands.Bot(command_prefix='', intents=discord.Intents.all())
 client = discord.Client()
 @client.event
 async  def on_ready():
@@ -9,5 +11,20 @@ async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('!Server'):
-        await message.channel.send(f'{message.author.mention} milkiway.aternos.me:58562')  
+        await message.channel.send(f'{message.author.mention} milkiway.aternos.me:58562')
+@bot.event
+async def on_ready():
+    print("Bot started")
+@bot.event
+async def on_message(self, message):
+    # so that the bot does not display its own messages
+    if message.author == bot.user:
+        return
+    # displaying messages from the chat
+    if message.content.startswith(''):
+        await message.channel.send(f'{message.author} написал {message.content}')
+@bot.event
+async def on_member_join(member):
+    role = member.guild.get_role(role_id=989847812755820558)
+    await member.add_roles(role)
 client.run(TOKEN, bot=True)
