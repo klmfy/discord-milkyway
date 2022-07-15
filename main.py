a6c7f0463318515039c1c425fb03894592428186
KlamFy <103330222+AleksKlamfy@users.noreply.github.com>
2022-07-15 20:20:11 +0300

    Update main.py
    fix bugs

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
 from discord.ext import commands
 import discord
 TOKEN = 'OTg5MTg1NzU1Njg1NDU4MDIw.G_VVxw.GYqbQfrwZKsEwO-TnbqAD5a4r8jSX4gNucPS6U'
-bot1 = commands.Bot(command_prefix='', intents=discord.Intents.all())
+bot = commands.Bot(command_prefix='', intents=discord.Intents.all())
 client = discord.Client()
-@client.event
-async  def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-@client.event
+@bot.event
 async def on_message(message):
     if message.author == client.user:
         return
     if message.content.startswith('!Server'):
         await message.channel.send(f'{message.author.mention} milkiway.aternos.me:58562')
-@bot1.event
+@bot.event
 async def on_ready():
     print("Bot started")
-@bot1.event
-async def on_message(self, message):
-    # so that the bot does not display its own messages
-    if message.author == bot.user:
-        return
-    # displaying messages from the chat
-    if message.content.startswith(''):
-        await message.channel.send(f'{message.author} написал {message.content}')
-@bot1.event
+@bot.event
 async def on_member_join(member):
-    role = member.guild.get_role(role_id=989847812755820558)
+    role = member.guild.get_role(role_id=950107065878188072)
     await member.add_roles(role)
-client.run(TOKEN, bot=True)
-bot1.run(TOKEN)
+bot.run(token)
+client.run(token, bot=True)
+
